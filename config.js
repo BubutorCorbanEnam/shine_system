2bc5cd136c3a6c883f3d71e54c4b5b78e197456d
BubutorCorbanEnam <126816112+BubutorCorbanEnam@users.noreply.github.com>
2026-05-18 10:48:12 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Never put the service_role key in this browser file.
 window.SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_PUBLIC_KEY';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZWRsYWV1a3Fnam9kZWVybHB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NzI2NDUsImV4cCI6MjA5MzE0ODY0NX0.Cy-Ye9hMdUd1mi5sPB-k8EWKmgwFaTi9_Zl6bJVB-UY';
